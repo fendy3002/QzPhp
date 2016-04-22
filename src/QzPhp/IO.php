72751bebf8423a9c13776188b0ebb7784f3d7a27
Fendy Heryanto <fendy.heryanto@coderscolony.com>
2016-04-22 16:04:51 +0700

    Added more IO operation

--- src/QzPhp/IO.php
+++ src/QzPhp/IO.php
@@ -18,6 +18,52 @@ class IO{
         return $result;
     }
 
+    public function zip($source, $saveAs){
+        // Get real path for our folder
+        $rootPath = realpath($source);
+
+        // Initialize archive object
+        $zip = new \ZipArchive();
+        $zip->open($saveAs, \ZipArchive::CREATE | \ZipArchive::OVERWRITE);
+
+        // Create recursive directory iterator
+        /** @var SplFileInfo[] $files */
+        $files = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator($rootPath),
+            \RecursiveIteratorIterator::LEAVES_ONLY
+        );
+
+        foreach ($files as $name => $file)
+        {
+            // Skip directories (they would be added automatically)
+            if (!$file->isDir())
+            {
+                // Get real and relative path for current file
+                $filePath = $file->getRealPath();
+                $relativePath = substr($filePath, strlen($rootPath) + 1);
+
+                // Add current file to archive
+                $zip->addFile($filePath, $relativePath);
+            }
+        }
+
+        // Zip archive will be created only after closing object
+        $zip->close();
+    }
+
+    public function deleteFolderContent($path){
+        $path = rtrim($path, DIRECTORY_SEPARATOR);
+        $files = glob($path . DIRECTORY_SEPARATOR . '*' ); // get all file names
+        foreach($files as $file){ // iterate files
+            if(is_dir($file)) {
+                $this->clearFolder($file);
+                rmdir($file);
+            } else {
+                unlink($file);
+            }
+        }
+    }
+
     public function getFolderSize($path){
         if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
             return $this->getFolderSizeWindows($path);
