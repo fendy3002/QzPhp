9d943ff89ed10523fed7bf86a16f01885d91d522
Fendy Heryanto <fendy.heryanto@coderscolony.com>
2016-06-02 19:16:37 +0700

    Remove additional field not used

--- src/QzPhp/DBConGenerator.php
+++ src/QzPhp/DBConGenerator.php
@@ -4,12 +4,6 @@ namespace QzPhp;
 class DBConGenerator{
     public function generate($host, $database, $user, $password){
         $dsn = 'mysql:dbname=' . $database . ';host=' . $host;
-        if(!empty($user)){
-            $dsn .= ';username=' . $user;
-        }
-        if(!empty($password)){
-            $dsn .= ';password=' . $password;
-        }
         return $dsn;
     }
 }
