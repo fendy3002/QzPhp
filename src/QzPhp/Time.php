87d844c3c12678424018eac1616902d0c019b8a5
Fendy Heryanto <fendy.heryanto@coderscolony.com>
2016-08-15 19:06:30 +0700

    Added between time

---
+++ src/QzPhp/Time.php
@@ -0,0 +1,30 @@
+<?php
+namespace QzPhp;
+use Carbon\Carbon;
+
+class Time{
+    public function between($nowStr, $fromStr, $toStr, $format = 'G.i'){
+        $from = Carbon::createFromFormat($format, $fromStr);
+        $to = Carbon::createFromFormat($format, $toStr);
+        $now = Carbon::createFromFormat($format, $nowStr);
+
+        if($from->gt($to)){
+            $now = $now->addDay();
+            $to = $to->addDay();
+            if($now->gte($from) && $now->lte($to)){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            if($now->gte($from) && $now->lte($to)){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+}
---
+++ tests/Time/BetweenTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\String;
+
+class BetweenTest extends \Tests\TestCase
+{
+    public function testNormal()
+    {
+        $time = new \QzPhp\Time();
+        $data = [
+            ["from" => "23.00", "to" => "08.00", 'now' => '06.00', 'expected' => true],
+            ["from" => "23.00", "to" => "08.00", 'now' => '07.00', 'expected' => true],
+            ["from" => "23.00", "to" => "08.00", 'now' => '11.00', 'expected' => false],
+            ["from" => "08.00", "to" => "23.00", 'now' => '06.00', 'expected' => false],
+            ["from" => "08.00", "to" => "23.00", 'now' => '11.00', 'expected' => true],
+            ["from" => "08.00", "to" => "23.00", 'now' => '23.30', 'expected' => false]
+        ];
+
+        foreach($data as $each){
+            $actual = $time->between($each['now'], $each['from'], $each['to']);
+            $this->assertEquals($each['expected'], $actual);
+        }
+    }
+}
