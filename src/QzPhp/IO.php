4d1d0a8374d8654e581cd514e9fa51dbdd524936
Fendy Heryanto <fendy.heryanto@coderscolony.com>
2017-06-21 16:06:53 +0700

    Change zip to be more flexible

--- src/QzPhp/IO.php
+++ src/QzPhp/IO.php
@@ -51,32 +51,52 @@ class IO{
      * @return void
      */
     public function zip($source, $saveAs){
-        // Get real path for our folder
-        $rootPath = realpath($source);
+        $sources = [];
+        if(!is_array($source)){
+            $sources = [$source];
+        }
+        else{
+            $sources = $source;
+        }
+
+        $files = [];
+        foreach($sources as $each){
+            // Get real path for our folder
+            $rootPath = realpath($each);
+
+            if(is_dir($rootPath)){
+                // Create recursive directory iterator
+                /** @var SplFileInfo[] $files */
+                $recursive = new \RecursiveIteratorIterator(
+                    new \RecursiveDirectoryIterator($rootPath),
+                    \RecursiveIteratorIterator::LEAVES_ONLY
+                );
+                foreach ($files as $name => $file)
+                {
+                    // Skip directories (they would be added automatically)
+                    if (!$file->isDir())
+                    {
+                        // Get real and relative path for current file
+                        $filePath = $file->getRealPath();
+                        $relativePath = substr($filePath, strlen($rootPath) + 1);
+
+                        // Add current file to archive
+                        $files[] = (object)["filePath" => $filePath, "saveAs" => $relativePath];
+                    }
+                }
+            }
+            else{
+                $files[] = (object)["filePath" => $rootPath, "saveAs" => basename($rootPath)];
+            }
+        }
 
         // Initialize archive object
         $zip = new \ZipArchive();
         $zip->open($saveAs, \ZipArchive::CREATE | \ZipArchive::OVERWRITE);
 
-        // Create recursive directory iterator
-        /** @var SplFileInfo[] $files */
-        $files = new \RecursiveIteratorIterator(
-            new \RecursiveDirectoryIterator($rootPath),
-            \RecursiveIteratorIterator::LEAVES_ONLY
-        );
-
-        foreach ($files as $name => $file)
+        foreach ($files as $file)
         {
-            // Skip directories (they would be added automatically)
-            if (!$file->isDir())
-            {
-                // Get real and relative path for current file
-                $filePath = $file->getRealPath();
-                $relativePath = substr($filePath, strlen($rootPath) + 1);
-
-                // Add current file to archive
-                $zip->addFile($filePath, $relativePath);
-            }
+            $zip->addFile($file->filePath, $file->saveAs);
         }
 
         // Zip archive will be created only after closing object
